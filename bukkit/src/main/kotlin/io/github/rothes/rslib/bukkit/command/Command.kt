d96bc95dd6227eaddba0b862559362ed7a674efc
Rothes <449181985@qq.com>
2023-03-18 22:31:46 +0800

    Better default values

--- bukkit/src/main/kotlin/io/github/rothes/rslib/bukkit/command/Command.kt
+++ bukkit/src/main/kotlin/io/github/rothes/rslib/bukkit/command/Command.kt
@@ -5,11 +5,17 @@ import org.bukkit.command.CommandSender
 class Command(
     override val name: String,
     override val execute: (CommandSender, Array<String>) -> ICommand.Result,
-    override val tab: (CommandSender, Array<String>) -> List<String>,
-    override val descriptionKey: String,
+    override val tab: (CommandSender, Array<String>) -> List<String> = { _, _ -> emptyList() },
     override val alias: Array<String> = emptyArray(),
     override val permission: String? = null,
-    override val parent: ICommand? = null
+    override val parent: ICommand? = null,
+    override val descriptionKey: String = "Commands" + StringBuilder().apply {
+        var cmd = parent
+        while (cmd != null) {
+            append('.').append(cmd!!.name)
+            cmd = cmd!!.parent
+        }
+    } + ".Description",
 ) : ICommand {
 
     override val children: MutableList<ICommand> = mutableListOf()
