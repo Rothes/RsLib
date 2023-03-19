da1d309bb38a3486466000fcd22abf3b41f42f17
Rothes <449181985@qq.com>
2023-03-20 01:20:27 +0800

    `Component.replace` accepts multiple replacements

--- bukkit/src/main/kotlin/io/github/rothes/rslib/bukkit/extensions/ComponentExtensions.kt
+++ bukkit/src/main/kotlin/io/github/rothes/rslib/bukkit/extensions/ComponentExtensions.kt
@@ -8,6 +8,17 @@ fun Component.replace(search: String, replacement: String): Component {
     return replaceText(TextReplacementConfig.builder().matchLiteral(search).replacement(replacement).build())
 }
 
+fun Component.replace(vararg replacements: String): Component {
+    if (replacements.size % 2 == 1)
+        throw IllegalArgumentException("Replacements size is not even")
+    var result = this
+    val iterator = replacements.iterator()
+    while (iterator.hasNext()) {
+        result = result.replace(iterator.next(), iterator.next())
+    }
+    return result
+}
+
 fun Component.replaceRaw(search: String, replacement: String): Component {
     return GsonComponentSerializer.gson().deserialize(GsonComponentSerializer.gson().serialize(this).replace(search, replacement))
 }
